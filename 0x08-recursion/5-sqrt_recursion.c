97b7bac35fc14c012ab43e334ac15a0980a5c20d
Lisah kamau <lisahkamau@gmail.com>
2023-11-07 09:43:52 -0800

    Add untracked files

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+int actual_sqrt_recursion(int n, int i);
+/**
+ * _sqrt_recursion - returns the natural square root of a number.
+ *
+ * @n: number to calculate the sqare root of
+ *
+ * Return: the resulting square root
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0 || n == 1)
+		return (n);
+	start = 1;
+	int end = n / 2;
+	int result = -1;
+
+	while (start <= end)
+	{
+		int mid = (start + end) / 2;
+
+		if (mid * mid == n)
+			return (mid);
+		else if (mid * mid < n)
+		{
+			start = mid + 1;
+			result = mid;
+		}
+		else
+			end = mid - 1;
+	}
+	return (result);
+}
