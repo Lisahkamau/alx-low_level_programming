0a8621afd589750013227199481302aca76f3b91
Lisahkamau <lisahkamau@gmail.com>
2023-11-16 13:02:23 -0800

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,10 +1,3 @@
-/*
- * File: print_list.c
- * Author: Your Name
- * Date: Date
- * Description: Print elements of a singly linked list
- */
-
 #include <stdio.h>
 #include "lists.h"
 
@@ -14,7 +7,7 @@
  *
  * Return: The number of nodes
  */
-size_t print_list(const struct list_s *h)
+size_t print_list(const list_t *h)
 {
 	size_t node_count = 0;
 
@@ -26,28 +19,5 @@ size_t print_list(const struct list_s *h)
 	}
 
 	return (node_count);
-}
-
-/**
- * main - Example usage of the print_list function
- * Return: Always 0
- */
-int main(void)
-{
-	/* Creating a sample linked list */
-	struct list_s node1 = {"Hello", 5, NULL};
-	struct list_s node2 = {"World", 5, NULL};
-	struct list_s node3 = {NULL, 0, NULL};
-
-	node1.next = &node2;
-	node2.next = &node3;
-
-	/* Printing the linked list and getting the number of nodes */
-	size_t count = print_list(&node1);
-
-	/* Printing the number of nodes */
-	printf("Number of nodes: %lu\n", count);
-
-	return (0);
 }
 
