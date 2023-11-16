ba8c7e0380f5620ffb2186188fd358e5dff47af9
Lisahkamau <lisahkamau@gmail.com>
2023-11-16 13:45:22 -0800

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,24 +2,18 @@
 
 /**
  * pre_main_message - Prints a message before the main function is executed.
+ *
+ * Description: This function is attributed with the constructor attribute,
+ * which ensures that it is executed before the main function.
+ */
+void pre_main_message(void) __attribute__((constructor));
+
+/**
+ * pre_main_message - Prints a message before the main function is executed.
  */
 void pre_main_message(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
-}
-
-/**
- * main - Entry point of the program.
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	pre_main_message();
-
-	/* Rest of your main function code */
-
-	return (0);
 }
 
